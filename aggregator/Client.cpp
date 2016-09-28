b86f6cb016f6805ee98dd8597cd1521f535d6d82
Nguyen Dinh Trung Truc <truc.ndtt@gmail.com>
2016-09-29 01:48:59 +0700

    Aggregator: prevent multiple subscription Client: Handle Session Initiation PowerHandler: Name thread

--- aggregator/Client.cpp
+++ aggregator/Client.cpp
@@ -11,6 +11,7 @@ Client::Client()
 {
 	mAggregator = NULL;
 	mSockFd = -1;
+	mUser = 0;
 }
 
 Client::~Client()
@@ -62,6 +63,78 @@ void ClientGSM::clientHandler()
 	delete this;
 }
 
+int Client::sessionInitiation(const char* aRcvMsg, int aRcvMsgSize)
+{
+	Json::Value response, root;
+	Json::Reader reader;
+	int8_t ret = 0;
+
+	if (reader.parse(aRcvMsg, aRcvMsg + aRcvMsgSize, root) == 0)
+	{
+		cout << "Error: Parse JSON failed" << endl;
+		return -1;
+	}
+
+	response["result"] = "SUCCESS";
+	response["desc"] = "Session initialized";
+
+	if (root.isMember("user") == false || root.isMember("pass") == false
+			|| root.isMember("action") == false)
+	{
+		ret = -3;
+	}
+	else if (root["action"].asString() != "SessionInitiation")
+	{
+		ret = -3;
+	}
+	else
+	{
+		string passwd = root["pass"].asString();
+
+		if (root["user"] == "admin")
+		{
+			if (mAggregator->compareAdminPwd(passwd) == false)
+			{
+				ret = -1;
+			}
+			else mUser = 1;
+		}
+		else if (root["user"] == "subscriber")
+		{
+			if (mAggregator->compareSubscriberPwd(passwd) == false)
+			{
+				ret = -1;
+			}
+			else mUser = 0;
+		}
+		else // user doesn't match
+		{
+			ret = -2;
+		}
+	}
+
+	if (ret < 0)
+	{
+		response["result"] = "FAILED";
+		if (ret == -1) response["desc"] = "Authentication failed";
+		else if (ret == -2) response["desc"] = "Wrong user";
+		else if (ret == -3) response["desc"] = "Parameter missing";
+	}
+
+	Json::FastWriter writer;
+	string outMsg = writer.write(response);
+
+	cout << "outMsg = " << outMsg << endl;
+	cout << "outMsg's size = " << outMsg.size() << endl;
+
+	if (send(mSockFd, outMsg.c_str(), outMsg.size() + 1, 0) != outMsg.size() + 1)
+	{
+		cout << "send() failed from: " << __FILE__ << ":" << __LINE__ << endl;
+	}
+
+	return ret;
+}
+
 int Client::receivedCmdHandler(const char* aRcvMsg, int aRcvMsgSize)
 {
 	Json::Reader reader;
@@ -75,15 +148,20 @@ int Client::receivedCmdHandler(const char* aRcvMsg, int aRcvMsgSize)
 
 	if (root["action"] == "AddSubscriber")
 	{
-		addSubscriberHdl(root);
+		return addSubscriberHdl(root);
+	}
+	else
+	{
+		cout << "ERR: Invalid command: " << aRcvMsg << endl;
 	}
 
 	return 0;
 }
 
-void Client::addSubscriberHdl(Json::Value &aRoot)
+int Client::addSubscriberHdl(Json::Value &aRoot)
 {
 	Json::Value response;
+	int8_t ret = 0;
 
 	response["result"] = "SUCCESS";
 	response["desc"] = "Added";
@@ -92,11 +170,13 @@ void Client::addSubscriberHdl(Json::Value &aRoot)
 	{
 		response["result"] = "FAILED";
 		response["desc"] = "Parameter missing";
+		ret = -1;
 	}
 	else if (subscribe(aRoot["status"].asString(), aRoot["phone"].asString()) < 0)
 	{
 		response["result"] = "FAILED";
 		response["desc"] = string(aRoot["status"].asString() + " is not supported");
+		ret = -1;
 	}
 
 	Json::FastWriter writer;
@@ -109,6 +189,8 @@ void Client::addSubscriberHdl(Json::Value &aRoot)
 	{
 		cout << "send() failed from: " << __FILE__ << ":" << __LINE__ << endl;
 	}
+
+	return ret;
 }
 
 ClientBLE::ClientBLE(Aggregator* anAggregator, int aSockFd)
@@ -131,6 +213,26 @@ void ClientBLE::clientHandler()
 	pthread_setname_np(pthread_self(), threadName);
 	cout << "Handling BLE client" << endl;
 
+	/* Session Initiation */
+	if ((rcvMsgSize = recv(mSockFd, rcvBuffer, RCV_BUF_SIZE, 0)) <= 0)
+	{
+		cout << "recv() failed from: " << __FILE__ << ":" << __LINE__ << endl;
+		cout << "rcvMsgSize = " << rcvMsgSize << endl;
+
+		delete this;
+		return;
+	}
+	//TODO: timeout
+
+	if (sessionInitiation(rcvBuffer, rcvMsgSize) < 0)
+	{
+		cout << "Cannot initiate connection\n";
+
+		delete this;
+		return;
+	}
+
+	/* Receive messages from client */
 	while (1)
 	{
 		if ((rcvMsgSize = recv(mSockFd, rcvBuffer, RCV_BUF_SIZE, 0)) <= 0)
@@ -143,6 +245,8 @@ void ClientBLE::clientHandler()
 		}
 
 		rcvBuffer[rcvMsgSize] = NULL;
-		printf("Received BLE: %s\n", rcvBuffer);
+		cout << "Received BLE: " << rcvBuffer << endl;
+
+		receivedCmdHandler(rcvBuffer, rcvMsgSize);
 	}
 }
